9c5f6eff4434ce6d65333d7235ebf397bd0ebe92
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-17 11:19:50 +0100

    using fetch to recieve data from server on client

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,11 +2,19 @@
 
 console.log('main.js is alive');
 
-const req = new XMLHttpRequest();
-req.open('GET', 'data');
-req.responseType = 'json';
-req.send();
+// const req = new XMLHttpRequest();
+// req.open('GET', 'data');
+// req.responseType = 'json';
+// req.send();
 
-req.onload = () => {
-    console.log(req.response);
+// req.onload = () => {
+//     console.log(req.response);
+// }
+
+const fetchData = fetch('/data')
+    .then(res => res.json())
+    .then(data => load(data));
+
+const load = (data) => {
+    console.log(data);
 }
