f20d6f6d869c29ef974b1af20af10a112845ae08
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-17 11:25:26 +0100

    alternative way of using fetch to get data from server

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,8 +11,19 @@ console.log('main.js is alive');
 //     console.log(req.response);
 // }
 
-const fetchData = fetch('/data')
-    .then(res => res.json())
+// const fetchData = fetch('/data')
+//     .then(res => res.json())
+//     .then(data => load(data));
+
+let myRequest = new Request('/data');
+
+fetch(myRequest)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => load(data));
 
 const load = (data) => {
