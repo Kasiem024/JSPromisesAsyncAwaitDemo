4a55c7ab06c28f43c1c303c5aafe4af9b8a06b7f
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-17 11:09:49 +0100

    using XHR to receive data from server on client

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('main.js is alive');
+
+const req = new XMLHttpRequest();
+req.open('GET', 'data');
+req.responseType = 'json';
+req.send();
+
+req.onload = () => {
+    console.log(req.response);
+}
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'JS Promises Async Await Demo' });
 });
 
+router.get('/data', function(req, res, next) {
+    res.send(require('../data/data.json'));
+});
+
 module.exports = router;
